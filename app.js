d3348332758ce6946c79c4c21e6405369e28f0d4
dtown1413 <dustin.vanhoute@gmail.com>
2023-04-12 15:29:33 +0900

    WIP

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+//variables required to identify values in html user inputs
 const dollarToPeso = document.querySelector('.usd-php');
 const dollarToWon = document.querySelector('.usd-won');
 const wonToPeso = document.querySelector('.won-php');
@@ -8,19 +8,37 @@ const pesoToDollar = document.querySelector('.php-usd');
 const userCurrency = document.querySelector('.userCurrency');
 const submit = document.querySelector('.submit');
 
-let dollarValue;
-let userValue;
-let solution;
+// Find user currency type in drop down menu and put in variable
+const userCurrencyDropdown = document.getElementById("userCurrencySelector");
+const selectedCurrency = userCurrencyDropdown.options[userCurrencyDropdown.selectedIndex].value;
 
-function convDollarToPeso() {
-    let dollarValue = parseFloat(dollarToPeso.value);
-    let userValue = parseFloat(userCurrency.value);
-    solution = dollarValue * userValue;
-};
+// Find user desired currency conversions based on checkbox selections in an array
+function getCheckedValues() {
+    let checkedValues = [];
+    let checkboxes = document.getElementsByTagName("input");
+    for (var i = 0; i < checkboxes.length; i++) {
+      if (checkboxes[i].type === "checkbox" && checkboxes[i].checked) {
+        checkedValues.push(checkboxes[i].value);
+      }
+    }
+    return checkedValues;
+  }
 
+//define the conversion required in an array
+function getConversionsRequired() {
+    let conversionsRequired = [];
+    for (var i = 0; i < checkedValues.length; i++) {
+        conversionRequired[i] = selectedCurrency + checkedValues[i];
+      }
+    return conversionsRequired
+    }
+
+//break out required conversion calculations
+
+//display value of conversion calculations in p html element at bottom of site
 submit.addEventListener('click', function() {
     event.preventDefault();
     convDollarToPeso();
     let output = document.getElementById("output");
-    output.textContent = "The result is " + solution;
+    output.textContent = conversionRequired;
 });
